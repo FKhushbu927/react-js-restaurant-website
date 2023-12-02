313ceb3f6209f743c1c338f0c409a7d968675eb8
FKhushbu927 <jannateekhushbu927@gmail.com>
2023-12-02 20:57:25 +0600

    featured section added

--- src/pages/Featured.jsx
+++ src/pages/Featured.jsx
@@ -2,20 +2,21 @@ import React from "react";
 import SectionTitle from "./SharedPages/SectionTitle";
 import featuredImg from "../../src/assets/featured.jpg";
 // import imgg from '../../src/assets/featuredimg.jpg'
+import './featured.css';
 
 const Featured = () => {
   return (
-    <div className="my-12">
-      <section>
+    <div className="my-12 featured-section py-8 text-white">
+      <section className="mt-5">
         <SectionTitle subheading={"Check it out"} heading={"FROM OUR MENU"} />
-      </section>
-      <div className="md:flex justify-center items-center">
+     
+      <div className="md:flex justify-center items-center gap-5 md:p-8 ">
         <div>
           <img src={featuredImg} alt="" />
         </div>
-        <div>
-          <p>November 2023</p>
-          <h4>WHERE CAN I GET SOME?</h4>
+        <div className="text-white">
+          <p className="text-xl font-medium py-2">November 2023</p>
+          <h4 className="text-2xl font-bold py-2">WHERE CAN I GET SOME ?</h4>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam,
             commodi Lorem ipsum dolor sit amet consectetur adipisicing elit. A
@@ -23,12 +24,13 @@ const Featured = () => {
             adipisci suscipit et.
           </p>
           <div className="pt-5">
-            <button className="btn btn-outline border-b-2 ">
+            <button className="btn btn-outline border-0 border-b-2">
               Read More
             </button>
           </div>
         </div>
       </div>
+      </section>
     </div>
   );
 };
